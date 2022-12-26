f0ae374ca4c5adf2a1e7891a86c853e50d2e911f
RomanKapustinskyi <kapustinskyiroman@gmail.com>
2022-12-26 12:33:55 +0200

    refactor

--- Assets/Scripts/AddForce.cs
+++ Assets/Scripts/AddForce.cs
@@ -1,39 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AddForce : MonoBehaviour
 {
-    private Rigidbody rb;
+    [SerializeField] private Transform cueParent;
+
+    private Rigidbody _rb;
+
     private void Awake()
     {
-        rb = GetComponent<Rigidbody>();
+        _rb = GetComponent<Rigidbody>();
     }
 
     private void Update()
     {
+        if (cueParent != null)
+        {
+            Vector3 pivotVector = Input.mousePosition - Camera.main.WorldToScreenPoint(cueParent.position);
+            float angle = Mathf.Atan2(pivotVector.y, pivotVector.x) * Mathf.Rad2Deg;
+            cueParent.rotation = Quaternion.AngleAxis(-angle - 180, Vector3.up);
+        }
+
         if(Input.GetKeyDown(KeyCode.Space))
         {
-            rb.AddForce(0f, 250, 0f);
+            _rb.AddForce(0f, 250, 0f);
         }
     }
     void FixedUpdate()
     {
         if(Input.GetKey(KeyCode.A))
         {
-            rb.AddForce(-5, 0f, 0f);
+            _rb.AddForce(-5, 0f, 0f);
         }
         if(Input.GetKey(KeyCode.D)) 
         {
-            rb.AddForce(5, 0f, 0f);
+            _rb.AddForce(5, 0f, 0f);
         }
         if(Input.GetKey(KeyCode.W))
         {
-            rb.AddForce(0f, 0f, 5);
+            _rb.AddForce(0f, 0f, 5);
         }
         if(Input.GetKey(KeyCode.S))
         {
-            rb.AddForce(0f,0f, -5);
+            _rb.AddForce(0f,0f, -5);
         }
     }
 }
